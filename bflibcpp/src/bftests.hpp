7f3c8cc0e783c2837cd34b5ee058a28cd89e9ae8
Brando <fong.m.brandon97@gmail.com>
2024-01-17 14:03:36 -0800

    creating bftests.hpp

---
+++ bflibcpp/src/bftests.hpp
@@ -0,0 +1,14 @@
+/**
+ * author: Brando
+ * date: 6/30/22
+ */
+
+#ifndef CPPLIB_TESTS_HPP
+#define CPPLIB_TESTS_HPP
+
+extern "C" {
+#include "../../bflibc/bin/debug/bflibc/bftests.h"
+}
+
+#endif // CPPLIB_TESTS_HPP
+
