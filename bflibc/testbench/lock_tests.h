caa63041695b0678ad70f98673ced9b5d39edef7
Brando <fong.m.brandon97@gmail.com>
2024-03-22 10:21:17 -0700

    new test

--- bflibc/testbench/lock_tests.h
+++ bflibc/testbench/lock_tests.h
@@ -50,6 +50,51 @@ int test_CreatingTimedWaitLock(void) {
 	return result;
 }
 
+typedef struct {
+	BFLock lock;
+	int i;
+} thread_test_waitinglock_struct;
+
+void thread_test_waitinglock(void * in) {
+	thread_test_waitinglock_struct * st = (thread_test_waitinglock_struct *) in;
+	st->i++;
+	BFLockRelease(&st->lock);
+}
+
+int test_waitinglock() {
+	UNIT_TEST_START;
+	int result = 0;
+
+	int max = 2 << 1;
+	while (!result && max--) {
+		thread_test_waitinglock_struct st;
+		st.i = 0;
+		result = BFLockCreate(&st.lock);
+
+		if (!result) {
+			if (!BFLockIsValid(&st.lock)) result = 1;
+		}
+
+		if (!result) {
+			BFThreadAsyncID tid = BFThreadAsync(thread_test_waitinglock, &st);
+			BFLockWait(&st.lock);
+
+			BFThreadAsyncWait(tid);
+
+			if (st.i == 0) {
+				result = 1;
+			}
+			BFThreadAsyncDestroy(tid);
+		}
+
+		if (!result) result = BFLockDestroy(&st.lock);
+	}
+
+	UNIT_TEST_END(!result, result);
+	return result;
+
+}
+
 void lock_tests(int * pass, int * fail) {
 	int p = 0, f = 0;
 
@@ -57,6 +102,7 @@ void lock_tests(int * pass, int * fail) {
 
 	LAUNCH_TEST(test_CreatingBFLock, p, f);
 	LAUNCH_TEST(test_CreatingTimedWaitLock, p, f);
+	LAUNCH_TEST(test_waitinglock, p, f);
 
 	if (pass) *pass += p;
 	if (fail) *fail += f;
