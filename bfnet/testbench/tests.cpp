f46669d66f6539147053f97eedbf89b5e54681aa
Brando <fong.m.brandon97@gmail.com>
2024-04-16 12:09:14 -0700

    Cleaning up testbench

--- bfnet/testbench/tests.cpp
+++ bfnet/testbench/tests.cpp
@@ -3,21 +3,13 @@
  * date: 2/15/24
  */
 
-#include "inputbuffer_tests.hpp"
-#include "chatroom_tests.hpp"
-#include "message_tests.hpp"
-#include "command_tests.hpp"
-#include "agent_tests.hpp"
-#include "log.hpp"
+extern "C" {
+#include <bflibc/bflibc.h>
+#include <bflibc/bftests.h>
+}
 
 int main() {
 	TEST_SUITE_START;
-
-	LAUNCH_TEST_SET(inputbuffer_tests);
-	LAUNCH_TEST_SET(chatroom_tests);
-	LAUNCH_TEST_SET(message_tests);
-	LAUNCH_TEST_SET(command_tests);
-	LAUNCH_TEST_SET(agent_tests);
 
 	TEST_SUITE_END;
 
