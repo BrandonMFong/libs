1dbb1b09e3fd8ca707988f103d33468707f64548
Brando <fong.m.brandon97@gmail.com>
2024-05-09 11:27:17 -0700

    good test

--- bflibc/testbench/stringutils_tests.h
+++ bflibc/testbench/stringutils_tests.h
@@ -8,6 +8,7 @@
 
 #include "clib_tests.h"
 #include <stringutils.h>
+#include <free.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -60,12 +61,24 @@ int test_uuidGen(void) {
 	return result;
 }
 
-void TestStringUtilsRandomFileCreate() {
+#define TEST_STRINGUTILS_RANDOM_FILE "/tmp/stringutils_tests_random_file"
+#define TEST_STRINGUTILS_RANDOM_TEXT "abcdefghijklmnopqrstuvwxz"
+#define TEST_STRINGUTILS_RANDOM_TEXT_OCC  (1024 * 20)
 
+void TestStringUtilsRandomFileCreate() {
+	FILE * f = fopen(TEST_STRINGUTILS_RANDOM_FILE, "w");
+
+	const char * buf = TEST_STRINGUTILS_RANDOM_TEXT;
+	int max = TEST_STRINGUTILS_RANDOM_TEXT_OCC;
+	while (max--) {
+		fwrite(buf, sizeof(char), strlen(buf), f);
+	}
+
+	fclose(f);
 }
 
 void TestStringUtilsRandomFileDelete() {
-
+	remove(TEST_STRINGUTILS_RANDOM_FILE);
 }
 
 int test_readingFile(void) {
@@ -73,6 +86,34 @@ int test_readingFile(void) {
 	int result = 0;
 
 	TestStringUtilsRandomFileCreate();
+
+	char * buf = BFStringCreateFromFile(TEST_STRINGUTILS_RANDOM_FILE);
+	int bufi = 0;
+	const char * randtext = TEST_STRINGUTILS_RANDOM_TEXT;
+	const size_t len = strlen(TEST_STRINGUTILS_RANDOM_TEXT);
+	for (int j = 0; j < TEST_STRINGUTILS_RANDOM_TEXT_OCC; j++) {
+		for (int i = 0; i < len; i++) {
+			if (bufi >= strlen(buf)) {
+				printf("\nbufi >= strlen buf\n");
+				result = 1;
+				goto endloop;
+			} else {
+				if (buf[bufi] != randtext[i]) {
+					printf("\n bufi=%d, i=%d, %c != %c\n",
+							bufi, i, buf[bufi], randtext[i]);
+					result = 1;
+					goto endloop;
+				}
+				bufi++;
+			}
+		}
+	}
+endloop:
+
+	if (buf == NULL)
+		result = 1;
+
+	BFFree(buf);
 	
 	TestStringUtilsRandomFileDelete();
 
