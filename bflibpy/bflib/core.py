7143ed4823fc74fe1e59096e32a3feb0bd7a55d3
Brando Fong <bfong@owc.com>
2024-05-13 10:29:44 -0700

    resetting core file

--- bflibpy/bflib/core.py
+++ bflibpy/bflib/core.py
@@ -1,12 +1,4 @@
 # author: Brando
 # date: 6/2/23
 
-def abserr(experimental, actual):
-    return abs(experimental - actual)
-
-def relerr(experimental, actual):
-    return abserr(experimental, actual) / actual
-
-def percerr(experimental, actual):
-    return relerr(experimental, actual) * 100
 
