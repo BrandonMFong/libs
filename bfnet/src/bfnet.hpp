defdaff5b8f427d5ec4d5c817e22db373714827b
Brando <fong.m.brandon97@gmail.com>
2024-05-20 13:42:16 -0700

    saving work

---
+++ bfnet/src/bfnet.hpp
@@ -0,0 +1,19 @@
+/**
+ * author: Brando
+ * date: 7/22/22
+ */
+
+#ifndef NETLIB_HPP
+#define NETLIB_HPP
+
+#include <bflibc/bflibcpp.h>
+
+#include "buffer.hpp"
+#include "client.hpp"
+#include "connection.hpp"
+#include "envelope.hpp"
+#include "server.hpp"
+#include "socket.hpp"
+
+#endif // NETLIB_HPP
+
--- makefiles/libpaths.mk
+++ makefiles/libpaths.mk
@@ -5,9 +5,12 @@
 BF_LIB_RPATH_DEBUG = bin/debug
 BF_LIB_RPATH_RELEASE = bin/release
 
-BF_LIB_RPATH_DEBUG_C = bin/debug/bflibc/libbfc-debug.a
-BF_LIB_RPATH_RELEASE_C = bin/release/bflibc/libbfc.a
+BF_LIB_RPATH_DEBUG_C = $(BF_LIB_RPATH_DEBUG)/bflibc/libbfc-debug.a
+BF_LIB_RPATH_RELEASE_C = $(BF_LIB_RPATH_RELEASE)/bflibc/libbfc.a
 
-BF_LIB_RPATH_DEBUG_CPP = bin/debug/bflibcpp/libbfcpp-debug.a
-BF_LIB_RPATH_RELEASE_CPP = bin/release/bflibcpp/libbfcpp.a
+BF_LIB_RPATH_DEBUG_CPP = $(BF_LIB_RPATH_DEBUG)/bflibcpp/libbfcpp-debug.a
+BF_LIB_RPATH_RELEASE_CPP = $(BF_LIB_RPATH_RELEASE)/bflibcpp/libbfcpp.a
+
+BF_LIB_RPATH_DEBUG_NET = $(BF_LIB_RPATH_DEBUG)/bflibcpp/libbfnet-debug.a
+BF_LIB_RPATH_RELEASE_NET = $(BF_LIB_RPATH_RELEASE)/bfnet/libbfnet.a
 
