fbf769fceb38c8b58419e7b4356f0eb72e49eb71
Brando Fong <bfong@owc.com>
2024-07-12 12:06:12 -0700

    making bftest sources

---
+++ bftest/src/bftests.h
@@ -0,0 +1,12 @@
+/**
+ * author: brando
+ * date: 7/12/24
+ */
+
+#ifndef BF_TESTS_H
+#define BF_TESTS_H
+
+
+
+#endif // BF_TESTS_H
+
---
+++ bftest/testbench/tests.c
@@ -0,0 +1,11 @@
+/**
+ * author: Brando
+ * date: 6/30/22
+ */
+
+#include <stdio.h>
+
+int main() {
+	return 0;
+}
+
