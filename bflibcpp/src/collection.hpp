60d7f8f80dd779ac5378884e0963065018bbbdab
Brando <fong.m.brandon97@gmail.com>
2024-11-07 11:03:26 -0800

    notes

--- bflibcpp/src/collection.hpp
+++ bflibcpp/src/collection.hpp
@@ -9,6 +9,13 @@
 #include "object.hpp"
 
 namespace BF {
+
+/**
+ * This class serves as a base class to all array type
+ * implementations. Purpose is to abstract each class such
+ * that I can have a set of mutable functions like sorting
+ * to be used across different classes
+ */
 class Collection : public Object {
 public:
 	~Collection();
