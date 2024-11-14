36733989e08764eca7c9bb47de591bfea4f06904
Brando <fong.m.brandon97@gmail.com>
2024-11-14 10:18:08 -0800

    saving work

--- bftest/src/bftest.h
+++ bftest/src/bftest.h
@@ -12,22 +12,40 @@
 #include <stdbool.h>
 
 /** TEST SUITE **/
+// a test suite is the high level function that will call
+// different sets of test coverage
+
+/**
+ * Call at the start of your test suite function
+ */
 #define TEST_SUITE_START \
 	int pass = 0, fail = 0;\
 	float tp = 0, tf = 0;
 
+/**
+ * Calls test coverage function
+ */
 #define TEST_SUITE_LAUNCH(foo) \
 	foo(&pass, &fail);\
 	tp += pass; tf += fail;\
 	pass = 0; fail = 0;
- 
+
+/**
+ * call at the end of your test suite function
+ */
 #define TEST_SUITE_END printf("Grade - %.2f%% (%d/%d)\n", (float) ((tp/(tp+tf)) * 100), (int) tp, (int) (tp+tf));
 
 /** TEST COVERAGE **/
 
+/**
+ * defines the test coverage function
+ */
 #define TEST_COVERAGE_FUNC(foo) \
 	void foo (int * pass, int * fail)
 
+/**
+ * initializes your test coverage function
+ */
 #define TEST_COVERAGE_START \
 	printf("---- %s started ----\n", __func__);\
 	int p = 0, f = 0;
@@ -39,6 +57,9 @@
 	if (!foo()) p++; \
 	else f++;
 
+/**
+ * ends your test your coverage function
+ */
 #define TEST_COVERAGE_END \
 	if (pass) *pass += p;\
 	if (fail) *fail += f;\
@@ -66,10 +87,19 @@
 	if (!(expr)) {\
 		result = -1;\
 		_BFTestLogPush("" __VA_ARGS__);\
-		UNIT_TEST_END \
+		UNIT_TEST_END; \
 	}
 
+/**
+ * adds a message to the test log
+ */
 void _BFTestLogPush(const char * format, ...);
+
+/**
+ * dumps all test log entries
+ *
+ * assumes all logs are error messages
+ */
 void _BFTestLogFlush(const char * suffix);
 
 #endif // BF_TEST_H
