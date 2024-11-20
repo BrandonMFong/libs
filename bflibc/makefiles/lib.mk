abf4aff738137bd4ec3bea31febad7a48e8400cc
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2024-11-20 11:13:50 -0800

    Updating BFLibC resources (#20)
    * using BF_LIB_C_FLAGS and putting all link flags needed for the library in it

--- bflibc/makefiles/lib.mk
+++ bflibc/makefiles/lib.mk
@@ -5,3 +5,11 @@ LIB_NAME = bflibc
 LIB_FILE_NAME_RELEASE = libbfc.a
 LIB_FILE_NAME_DEBUG = libbfc-debug.a
 
+# Determine the OS
+UNAME_S := $(shell uname -s)
+ifeq ($(UNAME_S),Linux)
+	BF_LIB_C_FLAGS += -luuid -lm
+endif
+ifeq ($(UNAME_S),Darwin)
+endif
+
--- bflibc/todo.md
+++ bflibc/todo.md
@@ -1,3 +1,5 @@
 ```
 [] sprintf is deprecated
+[x] link math lib for linux only
+[x] put all these flags into the libs file and couple it
 ```
