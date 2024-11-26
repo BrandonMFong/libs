8a48ce639cc87bc1a421ff4b51203d5b3f789e18
brandonmfong <fong.m.brandon97@gmail.com>
2024-11-26 00:41:34 -0800

    Adding note

--- makefiles/build.mk
+++ makefiles/build.mk
@@ -13,6 +13,7 @@ endif
 include $(LIBS_MAKEFILES_PATH)/libpaths.mk 
 include $(LIBS_MAKEFILES_PATH)/platforms.mk 
 
+# https://stackoverflow.com/a/18258352/12135693
 rwildcard=$(foreach d,$(wildcard $(1:=/*)),$(call rwildcard,$d,$2) $(filter $(subst *,%,$2),$d))
 
 UNAME_S := $(shell uname -s)
