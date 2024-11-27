7d3457bfe0e02c8df45e26e827d3e2bc183252dd
Brando <fong.m.brandon97@gmail.com>
2024-11-26 18:55:49 -0800

    updating Key and Value

--- bflibcpp/src/map.hpp
+++ bflibcpp/src/map.hpp
@@ -15,40 +15,52 @@ extern "C" {
 
 namespace BF {
 
-template <typename K, typename V, typename S = int>
+/**
+ * Map implemented using self-balancing tree. See bflibc/map.h
+ */
+template <typename K, typename V, typename S = size_t>
 class Map : public Object {
-	class Key : public Object {
+	template<typename T>
+	class Container : public Object {
 	public:
-		K _obj;
+		T _obj;
 		Map<K,V,S> * _mapRef;
-		Key(K obj, Map<K,V,S> * mapRef)
+		Container(T obj, Map<K,V,S> * mapRef)
 		: _obj(obj), _mapRef(mapRef), Object() {
 			BFRetain(this->_mapRef);
 		}
+		virtual ~Container() {
+			BFRelease(this->_mapRef);
+		}
+	};
+
+	template<typename T>
+	class Key : public Container<T> {
+	public:
+		Key(T obj, Map<K,V,S> * mapRef)
+		: Container<T>(obj, mapRef) { }
 		virtual ~Key() {
 			if (this->_mapRef->_releaseKey) {
 				this->_mapRef->_releaseKey(this->_obj);
 			}
-			BFRelease(this->_mapRef);
 		}
 	};
-	class Value : public Object {
+
+	template<typename T>
+	class Value : public Container<T> {
 	public:
-		V _obj;
-		Map<K,V,S> * _mapRef;
-		Value(V obj, Map<K,V,S> * mapRef)
-		: _obj(obj), _mapRef(mapRef), Object() {
-			BFRetain(this->_mapRef);
-		}
+		Value(T obj, Map<K,V,S> * mapRef)
+		: Container<T>(obj, mapRef) { }
 		virtual ~Value() {
 			if (this->_mapRef->_releaseValue) {
 				this->_mapRef->_releaseValue(this->_obj);
 			}
-			BFRelease(this->_mapRef);
 		}
 	};
-	friend class Key;
-	friend class Value;
+
+	friend class Key<K>;
+	friend class Value<V>;
+
 public:
 	Map()
 	: _compare(NULL), _releaseKey(NULL),
@@ -78,8 +90,8 @@ public:
 
 	int insert(K k, V v) {
 		if (!this->_map) return -1;
-		Key * key = new Key(k, this);
-		Value * value = new Value(v, this);
+		Key<K> * key = new Key<K>(k, this);
+		Value<V> * value = new Value<V>(v, this);
 		return BFMapInsert(this->_map, key, value);
 	}
 
@@ -88,7 +100,7 @@ public:
 		Key key(k, this);
 
 		int err = 0;
-		Value * value = (Value *) BFMapGetValue(this->_map, &key, &err);
+		Value<V> * value = (Value<V> *) BFMapGetValue(this->_map, &key, &err);
 		if (!value) {
 			if (error) *error = err;
 			return 0;
@@ -104,14 +116,14 @@ public:
 	int remove(K k) {
 		if (!this->_map) return -1;
 
-		Key key(k, this);
+		Key<K> key(k, this);
 		return BFMapRemove(this->_map, &key);
 	}
 
 	bool contains(K k) {
 		if (!this->_map) return false;
 
-		Key key(k, this);
+		Key<K> key(k, this);
 		return BFMapContains(this->_map, &key);
 	}
 
@@ -120,16 +132,16 @@ private:
 	void (*_releaseKey)(K obj);
 	void (*_releaseValue)(V obj);
 	static int _BFMapCompare(BFMapKey a, BFMapKey b) {
-		Key * akey = (Key *) a;
-		Key * bkey = (Key *) b;
+		Key<K> * akey = (Key<K> *) a;
+		Key<K> * bkey = (Key<K> *) b;
 		if (!akey || !bkey || !akey->_mapRef->_compare) {
 			return -1;
 		}
 		return akey->_mapRef->_compare(akey->_obj, bkey->_obj);
 	}
  	static void _BFMapRelease(BFMapKey k, BFMapValue v) {
-		Key * key = (Key *) k;
-		Value * value = (Value *) v;
+		Key<K> * key = (Key<K> *) k;
+		Value<V> * value = (Value<V> *) v;
 		BFRelease(key);
 		BFRelease(value);
 	}
