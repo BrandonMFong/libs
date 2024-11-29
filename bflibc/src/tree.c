c308e07bce50baef0253ff1ada23162985f97019
Brando <fong.m.brandon97@gmail.com>
2024-11-29 10:50:22 -0800

    removing in the release function

--- bflibc/src/tree.c
+++ bflibc/src/tree.c
@@ -33,11 +33,10 @@ void BFTreeReleaseNode(_BFTree * tree, _BFTreeNode * node) {
 	BFTreeReleaseNode(tree, node->left);
 	BFTreeReleaseNode(tree, node->right);
 
-	/*
 	if (tree->release) {
 		tree->release(node->object);
 	}
-	*/
+
 	_BFTreeNodeRelease(node);
 	tree->size--;
 }
