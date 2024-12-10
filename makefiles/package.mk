8f80b2d9edbae00b82f91836796ba9ed35404d14
Brando <fong.m.brandon97@gmail.com>
2024-12-09 17:03:25 -0800

    using package name as the volume name

--- makefiles/package.mk
+++ makefiles/package.mk
@@ -2,14 +2,22 @@
 # date: 12/9/24
 #
 
+UNAME_S := $(shell uname -s)
+ifeq ($(UNAME_S),Linux)
+PACKAGE_MODE = package-linux
+endif
+ifeq ($(UNAME_S),Darwin)
+PACKAGE_MODE = package-macos
+endif
+
 package: $(PACKAGE_MODE)
 
 package-linux: $(PACKAGE_NAME) $(PACKAGE_NAME)/$(BIN_NAME)
-	zip -r $(BIN_PATH)/$(PACKAGE_NAME)-$(PLATFORM).zip $(PACKAGE_NAME)
-	tar vczf $(BIN_PATH)/$(PACKAGE_NAME)-$(PLATFORM).tar.gz $(PACKAGE_NAME)
+	zip -r $(BIN_PATH)/$(PACKAGE_NAME).zip $(PACKAGE_NAME)
+	tar vczf $(BIN_PATH)/$(PACKAGE_NAME).tar.gz $(PACKAGE_NAME)
 
 package-macos: $(PACKAGE_NAME) $(PACKAGE_NAME)/$(BIN_NAME)
-	hdiutil create -fs HFS+ -volname Chat -srcfolder $(PACKAGE_NAME) $(BIN_PATH)/$(PACKAGE_NAME)-$(PLATFORM).dmg
+	hdiutil create -fs HFS+ -volname $(PACKAGE_NAME) -srcfolder $(PACKAGE_NAME) $(BIN_PATH)/$(PACKAGE_NAME).dmg
 
 $(PACKAGE_NAME):
 	mkdir -p $@
