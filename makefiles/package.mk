5d169487f1e46dbb3fee2e706a4e1d79fffcd4a6
Brando <fong.m.brandon97@gmail.com>
2024-12-10 11:31:21 -0800

    Adding notarize, codesign and stapling

--- makefiles/package.mk
+++ makefiles/package.mk
@@ -2,6 +2,18 @@
 # date: 12/9/24
 #
 
+ifeq ($(PACKAGE_BIN_TARGET),)
+$(error ERROR: "please define `PACKAGE_BIN_TARGET` in your makefile. These are files that are going to be packaged")
+endif
+
+ifeq ($(PACKAGE_NAME),)
+$(error ERROR: "please define `PACKAGE_NAME` in your makefile. This will define the name of the compressed package")
+endif
+
+ifeq ($(PACKAGE_BIN_PATH),)
+$(error ERROR: "please define `PACKAGE_BIN_PATH` in your makefile. This is where the package files will be outputed")
+endif
+
 UNAME_S := $(shell uname -s)
 ifeq ($(UNAME_S),Linux)
 PACKAGE_MODE = package-linux
@@ -24,4 +36,23 @@ $(PACKAGE_NAME):
 
 $(PACKAGE_NAME)/$(BIN_NAME): $(PACKAGE_BIN_PATH)/$(BIN_NAME)
 	@cp -afv $< $(PACKAGE_NAME)
+
+### codesigning
+
+codesign:
+	codesign -s "$(IDENTITY)" --options=runtime --timestamp $(PACKAGE_BIN_PATH)/$(BIN_NAME)
+
+### notarize
+
+notarize:
+	xcrun notarytool \
+	submit \
+	--apple-id "$(EMAIL)" \
+	--password "$(PW)" \
+	--team-id "$(TEAMID)" \
+	--wait \
+	$(PACKAGE_BIN_PATH)/$(PACKAGE_NAME).dmg
+
+staple:
+	xcrun stapler staple $(PACKAGE_BIN_PATH)/$(PACKAGE_NAME).dmg
 
