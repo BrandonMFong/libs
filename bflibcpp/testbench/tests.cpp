6d1588a0dd85cc3b88dbeae65320a3ea6dbb3e87
Brando <fong.m.brandon97@gmail.com>
2025-04-10 15:04:54 -0700

    adding back array

--- bflibcpp/testbench/tests.cpp
+++ bflibcpp/testbench/tests.cpp
@@ -33,8 +33,8 @@ BFTEST_SUITE_FUNC({
 	BFTEST_SUITE_LAUNCH(time_tests);
 	*/
 	BFTEST_SUITE_LAUNCH(data_tests);
-	/*
 	BFTEST_SUITE_LAUNCH(array_tests);
+	/*
 	BFTEST_SUITE_LAUNCH(vector_tests);
 	BFTEST_SUITE_LAUNCH(map_tests);
 	BFTEST_SUITE_LAUNCH(object_tests);
