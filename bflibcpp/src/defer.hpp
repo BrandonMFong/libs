b3b635eccae83b58e92d081e036bb1501b97298f
Brando <fong.m.brandon97@gmail.com>
2025-04-21 22:02:07 -0700

    defer update

--- bflibcpp/src/defer.hpp
+++ bflibcpp/src/defer.hpp
@@ -1,6 +1,14 @@
 /**
  * author: brando
  * date: 4/20/25
+ *
+ * abstract:
+ * Defer in other languages are used to defer logic when the scope exits. In c++, all object destructors are 
+ * called when the memory is going to go out of scope. I leverage this core functionality to 
+ * implement a defer block in c++. A class is defined to hold a callback function, which can be
+ * defined using c++ lambda syntax, and the class executes callback in its destructor. Using a macro,
+ * the use of the class and variable declaration is abracted. All that is needed is a macro call and a
+ * lambda block.
  */
 
 #ifndef DEFER_HPP
