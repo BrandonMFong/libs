23277649b533af0c49de9c56be0a6e0b29eb6a9e
Brando <fong.m.brandon97@gmail.com>
2025-05-04 10:10:20 -0700

    Good test

--- bflibcpp/testbench/tests.cpp
+++ bflibcpp/testbench/tests.cpp
@@ -22,16 +22,13 @@
 #include "defer_tests.hpp"
 
 BFTEST_SUITE_FUNC({
-	/*
 	BFTEST_SUITE_LAUNCH(atomic_tests);
 	BFTEST_SUITE_LAUNCH(string_tests);
 	BFTEST_SUITE_LAUNCH(stack_tests);
 	BFTEST_SUITE_LAUNCH(queue_tests);
 	BFTEST_SUITE_LAUNCH(time_tests);
 	BFTEST_SUITE_LAUNCH(data_tests);
-	*/
 	BFTEST_SUITE_LAUNCH(array_tests);
-	/*
 	BFTEST_SUITE_LAUNCH(vector_tests);
 	BFTEST_SUITE_LAUNCH(map_tests);
 	BFTEST_SUITE_LAUNCH(object_tests);
@@ -41,6 +38,5 @@ BFTEST_SUITE_FUNC({
 	BFTEST_SUITE_LAUNCH(tree_tests);
 	BFTEST_SUITE_LAUNCH(url_tests);
 	BFTEST_SUITE_LAUNCH(defer_tests);
-	*/
 })
 
