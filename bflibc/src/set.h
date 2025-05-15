3e38e1f1c447f611d1069b13df54bdeee3c66597
Brando <fong.m.brandon97@gmail.com>
2025-05-15 13:37:56 -0700

    documenting

--- bflibc/src/set.h
+++ bflibc/src/set.h
@@ -12,13 +12,46 @@
 typedef void * BFSet;
 typedef void * BFSetValue;
 
+/**
+ * creates set
+ *
+ * caller owns memory
+ */
 BFSet BFSetCreate();
+
+/**
+ * sets how the values are compared to one another
+ */
 void BFSetSetCompare(BFSet set, int (*compare)(BFSetValue a, BFSetValue b));
+
+/**
+ * how each value memory is handled once set is released
+ */
 void BFSetSetRelease(BFSet set, void (*release)(BFSetValue value));
+
+/**
+ * releases set from memory
+ */
 void BFSetRelease(BFSet set);
+
+/**
+ * inserts value into set
+ */
 int BFSetInsert(BFSet set, BFSetValue value);
+
+/**
+ * removes value from set
+ */
 int BFSetRemove(BFSet set, BFSetValue value);
+
+/**
+ * checks if value is in set
+ */
 bool BFSetContains(BFSet set, BFSetValue value);
+
+/**
+ * returns the current size of set
+ */
 size_t BFSetGetSize(BFSet set);
 
 #endif // SET_H
