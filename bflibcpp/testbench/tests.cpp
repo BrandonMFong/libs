ede2d9c7767cae0d86dcf59e49f81a3ecdc72e1f
Brando <fong.m.brandon97@gmail.com>
2025-05-27 19:09:25 -0700

    clean up

--- bflibcpp/testbench/tests.cpp
+++ bflibcpp/testbench/tests.cpp
@@ -25,7 +25,6 @@
 #include "removepointer_tests.hpp"
 
 BFTEST_SUITE_FUNC({
-	/*
 	BFTEST_SUITE_LAUNCH(atomic_tests);
 	BFTEST_SUITE_LAUNCH(string_tests);
 	BFTEST_SUITE_LAUNCH(stack_tests);
@@ -43,7 +42,6 @@ BFTEST_SUITE_FUNC({
 	BFTEST_SUITE_LAUNCH(url_tests);
 	BFTEST_SUITE_LAUNCH(set_tests);
 	BFTEST_SUITE_LAUNCH(hashset_tests);
-	*/
 	BFTEST_SUITE_LAUNCH(removepointer_tests);
 })
 
